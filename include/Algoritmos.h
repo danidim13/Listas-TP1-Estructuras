6412e7ee37a6bd72fbfcfd2cc94d9ab91fe033ca
DanielD <danidiaza@gmail.com>
2017-06-04 17:43:17 -0600

    Bugfix - Pasar parametros por referencia en algoritmo Sublista (Lista posicionada)

--- include/Algoritmos.h
+++ include/Algoritmos.h
@@ -12,7 +12,7 @@ template <typename E>
 void Invertir(ListaPos<E> &L);
 
 template <typename E>
-bool Sublista(ListaPos<E> L1, ListaPos<E> L2);
+bool Sublista(ListaPos<E> &L1, ListaPos<E> &L2);
 
 template <typename E>
 void BurbujaDoble(ListaPos<E> &L1);
